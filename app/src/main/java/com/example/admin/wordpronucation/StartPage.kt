003f5beeda0457c60877f12fd7560d7c26998cf9
Evgeny Motorin <zmotorin174@gmail.com>
2018-11-19 21:36:43 +0300

    init

---
+++ app/src/main/java/com/example/admin/wordpronucation/StartPage.kt
@@ -0,0 +1,15 @@
+package com.example.admin.wordpronucation
+
+import android.app.Activity
+import android.os.Bundle
+
+
+class StartPage : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_page)
+
+
+    }
+}
